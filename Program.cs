923af6212fd83a73c87b5d8b1c263b1d2b7a7b89
Joao Igor <joaoigor882@gmail.com>
2022-10-09 10:20:15 -0300

    Ajuste novamente,

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using MeufarmaceuticoApi.Repositories;
 using FastEndpoints.Swagger;
 using MeufarmaceuticoApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -13,12 +14,24 @@ builder.Services.AddScoped<ITreatmentRepository, TreatmentRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IDapperRepository, DapperRepository>();
 builder.Services.AddScoped<DataContext>();
-builder.Services.AddSwaggerDoc();
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(config.GetConnectionString("ServerConnection")));
 
 var app = builder.Build();
 
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 app.UseOpenApi();
 app.UseSwaggerUi3(s => s.ConfigureDefaults());
 
